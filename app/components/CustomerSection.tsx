6fb9dee453daffcba83efe48c17495761aec66b7
Anikwenze Emmanuel <milezkwen@gmail.com>
2025-05-02 14:47:27 +0100

    feat:landing-page-images

---
+++ app/components/CustomerSection.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomerSection = () => {
+  return (
+    <div>CustomerSection</div>
+  )
+}
+
+export default CustomerSection
---
+++ app/components/DirectOrders.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DirectOrders = () => {
+  return (
+    <div>DirectOrders</div>
+  )
+}
+
+export default DirectOrders
